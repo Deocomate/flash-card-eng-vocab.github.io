62dece5863b1e8e0cf3ef1269d3f7d890e988e41
Deocomate <deocomate@GMAIL.COM>
2023-05-06 15:15:44 +0700

    f

---
+++ assets/js/data.js
@@ -0,0 +1,127 @@
+var data = [
+    {
+        day:1,
+        vocabulary:[
+            {
+                word:"Service",
+                vnm:"Dịch vụ, sự phục vụ",
+                example:"This service is free to sb",
+                example_vnm:"Dịch vụ này miễn phí cho ai",
+            },
+            {
+                word:"Order",
+                vnm:"Đặt hàng, đơn hàng",
+                example:"Place an order",
+                example_vnm:"Đặt hàng",
+            },
+            {
+                word:"Provide",
+                vnm:"Cung cấp",
+                example:"Be provided with a free gift",
+                example_vnm:"Được tặng quà miễn phí",
+            },
+            {
+                word:"Offer",
+                vnm:"Đề nghị, đề xuất",
+                example:"Be offered daily",
+                example_vnm:"Được cung cấp hàng ngày",
+            },
+            {
+                word:"Completely",
+                vnm:"Hoàn toàn",
+                example:"Must be completed by + time",
+                example_vnm:"Phải được hoàn thiện trước + thời gian",
+            },
+            {
+                word:"Report",
+                vnm:"Báo cáo, bản báo cáo",
+                example:"Submit a progress report",
+                example_vnm:"Nộp bản báo cáo tiến độ",
+            },
+            {
+                word:"Last",
+                vnm:"Tiếp tục, kéo dài, cuối cùng",
+                example:"Last until May",
+                example_vnm:"Kéo dài đến thàng Năm",
+            },
+            {
+                word:"Schedule",
+                vnm:"Lịch trình, lên lịch trình",
+                example:"Arrive on schedule",
+                example_vnm:"Đến nơi theo đúng lịch",
+            },
+            {
+                word:"Following",
+                vnm:"Sau, tiếp theo",
+                example:"Following the tour of the local plant",
+                example_vnm:"Sau chuyến tham quan tại nhà máy địa phương",
+            },
+            {
+                word:"Position",
+                vnm:"Vị trí, tình hình, sắp xếp",
+                example:"A marketing position",
+                example_vnm:"Một vị trí trong bộ phận marketing",
+            },
+            {
+                word:"Require",
+                vnm:"Yêu cầu",
+                example:"Be required to submit receipts",
+                example_vnm:"Được yêu cầu nộp lại hóa đơn",
+            },
+            {
+                word:"Request",
+                vnm:"Đề nghị, yêu cầu",
+                example:"Make a request",
+                example_vnm:"Đưa ra đề nghị",
+            },
+            {
+                word:"Opening",
+                vnm:"Ngày thành lập, lễ khai trương",
+                example:"Job opening",
+                example_vnm:"Vị trí tuyển dụng",
+            },
+            {
+                word:"Review",
+                vnm:"Đánh giá, xem xét",
+                example:"Receive positive review for sb",
+                example_vnm:"Nhận được đánh giá tích cực từ ai",
+            },
+            {
+                word:"Increase",
+                vnm:"Tăng lên, sự tăng trưởng",
+                example:"A significant increase in sales",
+                example_vnm:"Tăng trưởng rõ rệt về doanh thu",
+            },
+            {
+                word:"Available",
+                vnm:"Có sẵng, có thể tìm được",
+                example:"Be available to work on weekends",
+                example_vnm:"Có thời gian làm việc vào cuối tuần",
+            },
+            {
+                word:"Need",
+                vnm:"Nhu cầu, cần",
+                example:"There is a need for",
+                example_vnm:"Có nhu cầu về",
+            },
+            {
+                word:"Local",
+                vnm:"Địa phương",
+                example:"Complaints from the local residents",
+                example_vnm:"Những phàn nàn của cư dân địa phương",
+            },
+            {
+                word:"Conference",
+                vnm:"Cuộc họp, hội nghị",
+                example:"At the press comference",
+                example_vnm:"Tại cuộc họp báo",
+            },
+            {
+                word:"Submit",
+                vnm:"Nộp, để xuất",
+                example:"Submit an application to sb",
+                example_vnm:"Nộp đơn ứng tuyển cho ai",
+            },
+        ]
+    }
+]
---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+
+
+    console.log(data);
+    let vocabList = data[0].vocabulary
+    let htmlVocab = ``;
+    for (const vocab of vocabList) {
+        htmlVocab += //html
+            `
+        <div class='item'>
+            <div class="contain-position">
+                <div class="flashcard-item front">
+                    <div class="px-4 px-md-5">
+                        <p class="mb-0"><b class="vocabulary fs-1">${vocab.word}</b></p>
+                        <p style="line-height: 25px;" class="example fs-3 text-center">
+                        ${vocab.example}
+                        </p>
+                    </div>
+                </div>
+                <div class="flashcard-item back">
+                    <div class="px-4 px-md-5">
+                        <p class="mb-0"><b class="vocabulary fs-1">${vocab.vnm}</b></p>
+                        <p style="line-height: 25px;" class="example fs-3 text-center">
+                            ${vocab.example_vnm}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    console.log(htmlVocab);
+    $(".flashcard-contain")[0].innerHTML = htmlVocab
+
+
+
+
+
+    initApp();
+});
+
+function renderFlashcard() {
+
+}
+
+function initApp() {
+    let colorList = [
+        "19A7CE",
+        "3C486B",
+        "D14D72",
+        "BA90C6",
+        "F99417",
+        "674188",
+        "7286D3",
+        "EB455F",
+        "7743DB",
+        "6D9886",
+        "E64848",
+    ]
+    let flashcard = $(".item").toArray()
+    for (const item of flashcard) {
+        let randomIndex = Math.floor(Math.random() * colorList.length)
+        let randomColor = "#" + colorList[randomIndex]
+        $(item).find(".flashcard-item").css({
+            "background-color": randomColor
+        })
+    }
+    $(".front").click(function (e) {
+        $(this).toggleClass("flip")
+        $(this).parent().find(".back").toggleClass("flip")
+    });
+    $(".back").click(function (e) {
+        $(this).toggleClass("flip")
+        $(this).parent().find(".front").toggleClass("flip")
+    });
+    $('.flashcard-contain').slick({
+        arrows:false
+    });
+}
